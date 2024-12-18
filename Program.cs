7901593978cfaa5e1af1d198ecbeb8ba991bcdda
CondesnsedMilk <achau103@student.hbuhsd.edu>
2024-12-18 21:02:08 +0000

    Program

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace unit_4_3_assignment_CondesnsedMilk;
+
+class Program
+{
+    float sum;
+    static void Main(string[] args)
+    {
+        
+        
+    
+
+    }
+
+    class Calculator()
+    {
+        Calculator sum = new Calculator();
+        float input = Convert.ToSingle(Console.Readline());
+    }
+}
+
